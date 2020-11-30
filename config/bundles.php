f088d4255fc63e3bd549e8e2363fe270fe04db51
Dawid Biernat <davd.biernat@gmail.com>
2020-11-30 23:10:51 +0100

    Init test & doctrine structure

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
